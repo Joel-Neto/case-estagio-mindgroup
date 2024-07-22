bf4ea7ab97d161cb46b9a0c1cfe6fed77318c45d
Joel Neto <joelalvesneto187@gmail.com>
2024-07-22 17:49:31 -0300

    Creating the routing files

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import { AppDataSource } from "./data-source";
-// import routes from "./routes";
+import transactionRoutes from "./routes/transactionRouter";
+import userRoutes from "./routes/userRouter";
+import categoryRoutes from "./routes/categoryRouter";
 
 AppDataSource.initialize().then(() => {
   const app = express();
 
   app.use(express.json());
 
-  // app.use(routes)
+  app.use("/transactions", transactionRoutes);
+  app.use("/routes", categoryRoutes);
+  app.use("/users", userRoutes);
 
   return app.listen(process.env.PORT);
 });
